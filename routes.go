6b120664a585dddae97e22048b659238701c5a09
mavensingh <kuldeep.avsar@gmail.com>
2020-03-21 22:04:58 +0530

    added new contact router

--- routes.go
+++ routes.go
@@ -6,6 +6,7 @@ func initializeRoutes() {
 
 	// Handle the index routes
 	router.GET("/", showIndexPage)
+	router.GET("/contact", showContactPage)
 
 	//Handle GET requests at article/view/some_article_id
 	router.GET("/article/view/:article_id", getArticle)
