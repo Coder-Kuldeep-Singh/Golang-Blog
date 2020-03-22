4eb2fca5b09904e24b66b1af95c65efc2a4cf270
mavensingh <kuldeep.avsar@gmail.com>
2020-03-22 13:23:42 +0530

    new router login

--- routes.go
+++ routes.go
@@ -7,6 +7,7 @@ func initializeRoutes() {
 	// Handle the index routes
 	router.GET("/", showIndexPage)
 	router.GET("/contact", showContactPage)
+	router.GET("/login", showLoginPage)
 
 	//Handle GET requests at article/view/some_article_id
 	router.GET("/article/view/:article_id", getArticle)
